f5dc45b987d610f3b1827c2f4e861b3c5537dd63
Jeevan Thandi <jeevan@airlatech.com>
2019-06-16 21:58:31 +0100

    project and pod init

---
+++ APIKit/APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by Jeevan Thandi on 16/06/2019.
+//  Copyright © 2019 Airla Tech Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
